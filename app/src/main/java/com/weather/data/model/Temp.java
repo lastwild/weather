4a3eb9961b91eae7028fd3df9b0bf342729a333a
olga <olga.dudkina@altarix.ru>
2015-12-23 18:07:01 +0400

    entity

---
+++ app/src/main/java/com/weather/data/model/Temp.java
@@ -0,0 +1,10 @@
+package com.weather.data.model;
+
+public class Temp {
+  public Float day;
+  public Float min;
+  public Float max;
+  public Float night;
+  public Float eve;
+  public Float morn;
+}
